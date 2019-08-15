62395875bfbb5c73790e54ee1c231e975255e746
Elad <elfifo4@gmail.com>
2019-08-15 23:53:01 +0300

    added methods to print rows as json: getRowsAsOriginalFormPrettyJson getRowsAsPrettyJson

--- library/src/main/java/com/eladfinish/googlesheet2json/model/SheetData.java
+++ library/src/main/java/com/eladfinish/googlesheet2json/model/SheetData.java
@@ -1,5 +1,10 @@
 package com.eladfinish.googlesheet2json.model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import java.util.ArrayList;
 
 public class SheetData {
@@ -63,6 +68,21 @@ public class SheetData {
         return rows;
     }
 
+
+    public String getRowsAsOriginalFormPrettyJson() {
+      return getPrettyJsonString(rows);
+    }
+
+    public String getRowsAsPrettyJson() {
+        //regex to match string of the form: "gsx$firstname":{"$t":"Elad"}
+        //noinspection RegExpRedundantEscape
+        String regex = "\"gsx\\$(.*?)\":\\{\"\\$t\":\"(.*?)\"\\}";
+
+        String simpleJson = new Gson().toJson(rows).replaceAll(regex, "\"$1\":\"$2\"");
+        return toPrettyFormat(simpleJson);
+    }
+
+
     @Override
     public String toString() {
         return "SheetData{" +
@@ -71,5 +91,43 @@ public class SheetData {
                 ", authors=" + authors +
                 ", rows=" + rows +
                 '}';
+    }
+
+
+    /**
+     * Convert an object to pretty print json version
+     *
+     * @param obj object to be converted to json
+     * @return pretty format of the json representation of the object
+     */
+    private static String getPrettyJsonString(Object obj) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonInString = gson.toJson(obj);
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(jsonInString);
+        return gson.toJson(jsonElement);
+    }
+
+
+    /**
+     * Convert a JSON string to pretty print version
+     *
+     * @param jsonString raw json as string (JsonObject or JsonArray)
+     * @return pretty format of the json
+     */
+    private static String toPrettyFormat(String jsonString) {
+        JsonParser parser = new JsonParser();
+
+        JsonElement jsonElement;
+        JsonElement parse = parser.parse(jsonString);
+        try {
+            jsonElement = parse.getAsJsonObject();
+        } catch (Exception e) {//java.lang.IllegalStateException: Not a JSON Object
+            // if jsonElement is not a JsonObject, try to parse it as a JsonArray
+            jsonElement = parse.getAsJsonArray();
+//            e.printStackTrace();
+        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(jsonElement);
     }
 }
