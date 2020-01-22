86b461cdd633031df5fc830d2123162becdd81e7
Elad <elfifo4@gmail.com>
2020-01-22 22:57:30 +0200

    get rid of Glide warning in sample app

---
+++ app/src/main/java/com/eladfinish/googlesheet2json/CustomGlideModule.java
@@ -0,0 +1,7 @@
+package com.eladfinish.googlesheet2json;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class CustomGlideModule extends AppGlideModule {}
